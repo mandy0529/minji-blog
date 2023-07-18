7b76637159c92a2d15ebb709ae1c5bfb3df36d21
minji <321963741@naver.com>
2023-07-18 09:16:18 +0900

    app module, controller, service setup & main.ts

---
+++ api/src/app.module.ts
@@ -0,0 +1,28 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './middleware';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { BlogModule } from './blog/blog.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+    UserModule,
+    BlogModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ api/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // global prefix middleware
+  app.setGlobalPrefix('/api/v1');
+
+  // cors
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  // validation middleware
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(3333);
+}
+bootstrap();
