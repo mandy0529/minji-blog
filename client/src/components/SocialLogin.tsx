15a1a7fe1f6279ce7df14f28b83568a4c76c3cd4
minji <321963741@naver.com>
2023-07-18 10:17:36 +0900

    로그인 성공하면 router.push 홈으로 보내기

--- client/src/components/SocialLogin.tsx
+++ client/src/components/SocialLogin.tsx
@@ -1,11 +1,21 @@
 "use client";
+
 import { tokenType } from "@/app/types";
 import { setCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const SocialLogin = ({ refresh_token, access_token }: tokenType) => {
+  // router
+  const router = useRouter();
+
   // cookie 설정
   setCookie("accessToken", access_token);
   setCookie("refreshToken", refresh_token);
+
+  useEffect(() => {
+    router.push("/");
+  }, []);
 
   return <div>Social-login</div>;
 };
