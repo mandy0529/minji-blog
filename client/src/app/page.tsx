f0ad80924b36f8296c0ac8fe4a705f67f32eb7a2
minji <321963741@naver.com>
2023-07-19 13:46:27 +0900

    dashboard, home UI basic init

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>home</Button>
+      home
     </main>
   );
 }
