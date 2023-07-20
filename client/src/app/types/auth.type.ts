0d09a81a80d122b5ce43e244bfc4972e759fc0b6
minji <321963741@naver.com>
2023-07-20 17:21:30 +0900

    signup, login type added

---
+++ client/src/app/types/auth.type.ts
@@ -0,0 +1,11 @@
+export type signupType = {
+  email: string;
+  password: string;
+  name: string;
+  role: string;
+};
+
+export type loginType = {
+  email: string;
+  password: string;
+};
--- client/src/app/types/index.ts
+++ client/src/app/types/index.ts
@@ -1,1 +1,2 @@
 export * from "./token.type";
+export * from "./auth.type";
