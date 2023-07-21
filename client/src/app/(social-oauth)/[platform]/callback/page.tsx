69ea6ea7bb69a611ce6129487b95a4b3a8dedade
minji <321963741@naver.com>
2023-07-21 11:29:38 +0900

    Feat: social login error handling logic

--- client/src/app/(social-oauth)/[platform]/callback/page.tsx
+++ client/src/app/(social-oauth)/[platform]/callback/page.tsx
@@ -8,15 +8,13 @@ async function RedirectPage(props: any) {
   } = props;
 
   // google callback page에서 getAccessToken()를 호출 => 로그인한 유저 token get
+
   const result = await getAccessToken(code, platform);
 
   return (
     <div>
       {/* props socialLogin에 넘겨주기 */}
-      <SocialLoginLoading
-        refresh_token={result.refresh_token}
-        access_token={result.access_token}
-      />
+      <SocialLoginLoading result={...result} />
     </div>
   );
 }
--- client/src/components/SocialLoginLoading.tsx
+++ client/src/components/SocialLoginLoading.tsx
@@ -1,29 +1,37 @@
 "use client";
 
 import { tokenType } from "@/app/types";
+import { toast } from "@/hook";
 import { setCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { Progress } from "./ui";
 
-export const SocialLoginLoading = ({
-  refresh_token,
-  access_token,
-}: tokenType) => {
+export const SocialLoginLoading = (result: any) => {
   // router
   const router = useRouter();
 
-  // cookie 설정
-  setCookie("accessToken", access_token);
-  setCookie("refreshToken", refresh_token);
+  if (result.result?.error) {
+    toast({
+      title: "There was a problem",
+      description: `${result.result?.message}`,
+      variant: "destructive",
+    });
+  }
 
+  // cookie 설정
+  setCookie("accessToken", result.result?.access_token);
+  setCookie("refreshToken", result.result?.refresh_token);
+  // toast({
+  //   title: "success",
+  //   description: "successfully logined",
+  // });
   useEffect(() => {
     router.push("/");
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-center mb-4">...progress in logging in</div>
+      <div className="text-center mb-4">...progress in logging in with</div>
     </div>
   );
 };
