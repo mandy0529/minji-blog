d8834cb2bd7fe25475e23c2595fffa86a6279f58
minji <321963741@naver.com>
2023-07-24 14:51:50 +0900

    feat: markdown logic * style 변경

---
+++ client/src/app/(blog)/blog/[id]/page.tsx
@@ -0,0 +1,79 @@
+'use client';
+import { blogAPI } from '@/api/blog';
+import { toast } from '@/hook';
+import { useQuery } from '@tanstack/react-query';
+import { Button, Skeleton } from '@/components/ui';
+import { BlogSkeleton } from '@/components/skeleton/blog.skeleton';
+import UserAvatar from '@/components/navbar/UserAvatar';
+import Markdown from 'markdown-to-jsx';
+import { koreanDateTime } from '@/utils/convert-korean-time';
+
+type singleDataType = {
+  author: {
+    email: string;
+    profile: string;
+  };
+  content: string;
+  id: number;
+  title: string;
+  createdAt: Date;
+  tag: string[];
+};
+
+const page = ({ params }: { params: { id: number } }) => {
+  // tanstack query
+  const { isLoading, error, data } = useQuery<singleDataType>({
+    queryKey: ['getSingleBlog'],
+    queryFn: () => blogAPI.getSingleBlog(Number(params.id)),
+  });
+
+  // error 있을때 error toast
+  if (error) {
+    toast({
+      title: 'Failed to get Blog',
+      //  @ts-ignore
+      description: `${error?.response?.data?.message}`,
+      variant: 'destructive',
+    });
+  }
+
+  if (!data) return;
+
+  const { content, id, author, title, tag, createdAt } = data;
+
+  return (
+    <div className="mt-20 place-content-center">
+      {isLoading ? (
+        <BlogSkeleton />
+      ) : (
+        <>
+          {/* user info */}
+          <div className="flex items-center mb-10">
+            <UserAvatar className="mr-3" user={author} />
+            <div className="font-bold text-lg">
+              {author.email?.split('@')[0]}님의 글
+            </div>
+            <div className="ml-3 text-gray-500 dark:text-gray-200">
+              {koreanDateTime(createdAt)}
+            </div>
+          </div>
+
+          {/* blog info */}
+          <div>
+            {tag?.map((item) => (
+              <Button className="mr-4" variant={'outline'}>
+                {item}
+              </Button>
+            ))}
+          </div>
+          <div className="text-3xl font-bold my-5">{title}</div>
+          <article className="prose prose-slate dark:prose-invert">
+            <Markdown>{content}</Markdown>
+          </article>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default page;
--- client/src/app/(blog)/blog/page.tsx
+++ client/src/app/(blog)/blog/page.tsx
@@ -1,12 +1,12 @@
-"use client";
-import { blogAPI } from "@/api/blog";
-import UserAvatar from "@/components/navbar/UserAvatar";
-import { Button } from "@/components/ui";
-import { toast } from "@/hook";
-import { useQuery } from "@tanstack/react-query";
-import { ChevronRight } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+'use client';
+import { blogAPI } from '@/api/blog';
+import UserAvatar from '@/components/navbar/UserAvatar';
+import { Button } from '@/components/ui';
+import { toast } from '@/hook';
+import { useQuery } from '@tanstack/react-query';
+import { ChevronRight } from 'lucide-react';
+import Markdown from 'markdown-to-jsx';
+import Link from 'next/link';
 
 export interface BlogType {
   id: number;
@@ -26,18 +26,18 @@ export interface Author {
 
 const page = () => {
   // tanstack query
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["getAllBlogs"],
+  const { isLoading, error, data, isInitialLoading } = useQuery({
+    queryKey: ['getAllBlogs'],
     queryFn: () => blogAPI.getAllBlogs(),
   });
 
   // error 있을때 error toast
   if (error) {
     toast({
-      title: "Failed to get Blog",
+      title: 'Failed to get Blog',
       //  @ts-ignore
       description: `${error?.response?.data?.message}`,
-      variant: "destructive",
+      variant: 'destructive',
     });
   }
 
@@ -49,20 +49,27 @@ const page = () => {
         return (
           <div
             key={id}
-            className="max-w-sm p-6 my-5 mx-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            className="relative max-w-sm p-6 my-5 mx-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
             <div className="flex items-center">
               <UserAvatar user={author} />
               <h5 className="mb-2 ml-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {title.length > 10 ? `${title.slice(0, 10)}...` : title}
+                {title.length > 10 ? `${title.slice(0, 10)}...` : title}
               </h5>
             </div>
 
-            <p className="my-5 font-normal text-gray-700 dark:text-gray-400">
-              {content.length > 30 ? content.slice(0, 30) : content}
-            </p>
+            <article className="prose prose-slate my-5 pb-5 dark:prose-invert">
+              <Markdown className="my-5 font-normal text-gray-700 dark:text-gray-400">
+                {content.length > 30 ? content.slice(0, 50) : content}
+              </Markdown>
+            </article>
+
             <Link href={`/blog/${id}`}>
-              <Button isLoading={isLoading}>
+              <Button
+                className="absolute m-2 bottom-0 left-0"
+                variant={'subtle'}
+                isLoading={isLoading}
+              >
                 Read more
                 <ChevronRight />
               </Button>
